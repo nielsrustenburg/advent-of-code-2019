89d68b50403ef49b16be80382ee5c66934d763cb
Niels Rustenburg <niels.rustenburg@gmail.com>
2020-02-04 12:07:18 +0100

    clean up Day2Tests

--- AoC/Tests/Day2Tests.cs
+++ AoC/Tests/Day2Tests.cs
@@ -14,33 +14,20 @@ namespace AoC.Tests
             Console.WriteLine("Day2Tests Completed!");
         }
 
+        static string CreateAndRunIntCode(IEnumerable<int> inputCode)
+        {
+            IntCode ic = new IntCode(inputCode);
+            ic.Run();
+            return ic.ToString();
+        }
+
         static void TestIntCode()
         {
-            void TestOutcome(IntCode ic, int expectedOutput)
-            {
-                string input = ic.ToString();
-                 ic.Run();
-                int output = ic.GetValAtMemIndex(0);
-                if (output != expectedOutput) throw new Exception($"{input}: {output}, expected {expectedOutput}");
-            }
-            void Test(IntCode ic, List<int> expectedOutput)
-            {
-                string input = ic.ToString();
-                ic.Run();
-                string output = ic.ToString();
-                string expOut = new IntCode(expectedOutput).ToString();
-                if (output != expOut) throw new Exception($"{input}: {output}, expected {expOut}");
-            }
-            IntCode ic1 = new IntCode(new List<int> { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 }); //Add->Multiply
-            IntCode ic2 = new IntCode(new List<int> { 1, 0, 0, 0, 99 }); //Add
-            IntCode ic3 = new IntCode(new List<int> { 2, 3, 0, 3, 99 }); //Multiply
-            IntCode ic4 = new IntCode(new List<int> { 2, 4, 4, 5, 99, 0 }); //Multiply
-            IntCode ic5 = new IntCode(new List<int> { 1, 1, 1, 4, 99, 5, 6, 0, 99 }); //Add->Multiply
-            TestOutcome(ic1, 3500);
-            Test(ic2, new List<int> { 2, 0, 0, 0, 99 });
-            Test(ic3, new List<int> { 2, 3, 0, 6, 99 });
-            Test(ic4, new List<int> { 2, 4, 4, 5, 99, 9801 });
-            Test(ic5, new List<int> { 30, 1, 1, 4, 2, 5, 6, 0, 99 });
+            TestSuite.Test("IC[3500,9,10,70,2,3,11,0,99,30,40,50]", new List<int> { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 }, CreateAndRunIntCode); //Add->Multiply
+            TestSuite.Test("IC[2,0,0,0,99]", new List<int> { 1, 0, 0, 0, 99 }, CreateAndRunIntCode); //Add
+            TestSuite.Test("IC[2,3,0,6,99]", new List<int> { 2, 3, 0, 3, 99 }, CreateAndRunIntCode); //Multiply
+            TestSuite.Test("IC[2,4,4,5,99,9801]", new List<int> { 2, 4, 4, 5, 99, 0 }, CreateAndRunIntCode); //Multiply
+            TestSuite.Test("IC[30,1,1,4,2,5,6,0,99]" , new List<int> { 1, 1, 1, 4, 99, 5, 6, 0, 99 }, CreateAndRunIntCode); //Add->Multiply
         }
 
         static void TestSolvePartOne()
