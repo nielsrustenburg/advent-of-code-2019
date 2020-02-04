b43bc5955f4d6b4fffa4ff939b09cea082dc97c9
Niels Rustenburg <niels.rustenburg@gmail.com>
2020-02-04 12:07:08 +0100

    Add test for functions that return IEnumerables

--- AoC/Tests/TestSuite.cs
+++ AoC/Tests/TestSuite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using System.Linq;
 
 namespace AoC.Tests
 {
@@ -53,6 +54,40 @@ namespace AoC.Tests
         {
             string output = function(input).ToString();
             if (expected.ToString() != output) throw new Exception($"{function.Method.Name} failed: \n expected value: \n {expected.ToString()} \n received value \n {output}");
+        }
+
+        public static void TestSequence<T,V>(IEnumerable<T> expected, V input, Func<V,IEnumerable<T>> function)
+        {
+            List<T> output = function(input).ToList();
+            List<T> expList = expected.ToList();
+            string errorMessage = "function.Method.Name} failed: \n ";
+            bool equalLength = output.Count == expList.Count;
+
+            IEnumerable<int> missingIndices = null;
+            if (!equalLength)
+            {
+                errorMessage = errorMessage + ($"expected output length: \n {expList.Count} \n actual output length \n {output.Count} \n");
+                missingIndices = Enumerable.Range(Math.Min(output.Count, expList.Count), Math.Max(output.Count, expList.Count) - Math.Min(output.Count, expList.Count));
+            }
+
+            string indexErrorMessage = "Mismatch at indices: ";
+            bool equalContent = equalLength;
+            for (int i = 0; i < Math.Min(output.Count, expList.Count); i++)
+            {
+                bool equalAtIndex_i = output[i].Equals(expList[i]);
+                if (!equalAtIndex_i)
+                {
+                    indexErrorMessage = indexErrorMessage + $"{i}, ";
+                }
+                equalContent = equalContent && equalAtIndex_i;
+            }
+
+            if (!equalContent)
+            {
+                errorMessage = errorMessage + indexErrorMessage;
+                errorMessage = errorMessage + string.Join(',', missingIndices.Select(x => x.ToString()));
+                throw new Exception(errorMessage);
+            }
         }
 
         public static string GetCurrentMethod()
