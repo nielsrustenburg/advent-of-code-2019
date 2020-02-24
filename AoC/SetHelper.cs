334b4883c9132f62db824f8a3a7ca7edab1c05e4
nielsrustenburg <niels.rustenburg@gmail.com>
2020-02-24 14:28:46 +0100

    create SetHelper class for basic operations from set-theory

---
+++ AoC/SetHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC
+{
+    static class SetHelper
+    {
+        public static List<List<T>> AsSingletons<T>(List<T> originalSet)
+        {
+            List<List<T>> singletons = originalSet.Select(element => new List<T> { element }).ToList();
+            return singletons;
+        }
+
+        public static List<List<T>> Permutations<T>(List<T> values)
+        {
+            List<List<T>> result = values.Count == 0 ? new List<List<T>> { new List<T>() } : new List<List<T>>();
+            foreach (T val in values)
+            {
+                List<List<T>> without = Permutations(values.Where(x => !x.Equals(val)).ToList());
+                foreach (List<T> setWithout in without)
+                {
+                    setWithout.Add(val);
+                    result.Add(setWithout);
+                }
+            }
+            return result;
+        }
+    }
+}
