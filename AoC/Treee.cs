4247bf98c24ce0e637dfe5d4581ea3ba04e34fab
nielsrustenburg <niels.rustenburg@gmail.com>
2020-02-25 13:08:57 +0100

    add interfaces and abstract class for trees and treenodes
    naming is temporary while I factor out my old implementation

---
+++ AoC/Treee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC
+{
+    abstract class Treee<N> where N : TreaNode<N>
+    {
+        protected Dictionary<string, N> nodes;
+
+        protected Treee()
+        {
+            nodes = new Dictionary<string, N>();
+        }
+
+        public void AddRelation(string parent, string child)
+        {
+            N pNode = GetOrCreateNode(parent);
+            N cNode = GetOrCreateNode(child);
+            pNode.AddChild(cNode);
+        }
+
+        public abstract N CreateNode(string id);
+
+        public N GetOrCreateNode(string id)
+        {
+            if (!nodes.TryGetValue(id, out N node))
+            {
+                node = CreateNode(id);
+                nodes[id] = node;
+            }
+            return node;
+        }
+    }
+}
