5397862ccdadf7789a0df6ff17f0956c703a9bc4
nielsrustenburg <niels.rustenburg@gmail.com>
2020-02-27 14:08:09 +0100

    complete refactor Day10 code
    Old code was way too messy to read did almost a complete rework. Used knowledge of part 2 to make both parts use similar structure to find solution

--- AoC/Days/Day10.cs
+++ AoC/Days/Day10.cs
@@ -7,289 +7,162 @@ namespace AoC
 {
     static class Day10
     {
+
         public static int SolvePartOne()
         {
             string[] input = InputReader.StringsFromTxt("d10input.txt");
-            char[,] asteroids = new char[input[0].Length, input.Length];
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                char[] chars = input[i].ToCharArray();
-                for (int j = 0; j < chars.Length; j++)
-                {
-                    asteroids[j, i] = chars[j];
-                }
-            }
-
-            int width = asteroids.GetLength(0);
-            int height = asteroids.GetLength(1);
-
-            int[,] viewCounts = new int[width, height];
-
-            int mostVisible = 0;
-            int bestx = -1;
-            int besty = -1;
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (asteroids[x, y] == '#')
-                    {
-                        bool[,] checkedAsteroids = new bool[width, height];
-                        checkedAsteroids[x, y] = true;
-                        for (int otherx = 0; otherx < width; otherx++)
-                        {
-                            for (int othery = 0; othery < height; othery++)
-                            {
-                                if (!checkedAsteroids[otherx, othery] && asteroids[otherx, othery] == '#')
-                                {
-                                    //Pretend this one as visible, even if its not
-                                    //Find all other positions on the same line of sight and mark them checked (these can no longer be counted visible, even if they are)
-                                    viewCounts[x, y]++;
-                                    List<(int, int)> losPoints = GetLineOfSight((x, y), (otherx, othery), (width, height));
-                                    foreach ((int x, int y) lp in losPoints)
-                                    {
-                                        checkedAsteroids[lp.x, lp.y] = true;
-                                    }
-                                }
-                            }
-                        }
-                        if (viewCounts[x, y] > mostVisible)
-                        {
-                            mostVisible = viewCounts[x, y];
-                            bestx = x;
-                            besty = y;
-                        }
-                    }
-                }
-            }
-
-            PrintViewCounts(viewCounts);
-            Console.WriteLine($"({bestx},{besty})");
-            return mostVisible;
-        }
-
-        private static void PrintViewCounts(int[,] viewCounts)
-        {
-            for (int i = 0; i < viewCounts.GetLength(0); i++)
-            {
-                string bob = "";
-                for (int j = 0; j < viewCounts.GetLength(1); j++)
-                {
-                    bob = bob + (viewCounts[i, j] > 0 ? viewCounts[i, j].ToString() : ".");
-                }
-                Console.WriteLine(bob);
-            }
-        }
-
-        private static List<(int, int)> GetLineOfSight((int x, int y) p1, (int x, int y) p2, (int x, int y) limits)
-        {
-            List<(int, int)> losPoints = new List<(int, int)> { };
-
-            (int x, int y) diff = (Math.Abs(p1.x - p2.x), Math.Abs(p1.y - p2.y));
-
-            //Find smallest angle
-            int divBy;
-            int smallestDiff = diff.x < diff.y ? diff.x : diff.y;
-            if (smallestDiff == 0)
-            {
-                divBy = Math.Max(diff.x, diff.y);
-            }
-            else
-            {
-                divBy = 1;
-                for (int i = 2; i <= smallestDiff; i++)
-                {
-                    if (diff.x % i == 0 && diff.y % i == 0)
-                    {
-                        divBy = i;
-                    }
-                }
-            }
-
-            (int x, int y) angle = ((p1.x - p2.x) / divBy, (p1.y - p2.y) / divBy);
-
-            //bool withinBoundsPlus = true;
-            bool withinBoundsMinus = true;
-            int c = 1;
-            while (/*withinBoundsPlus ||*/ withinBoundsMinus)
-            {
-                //withinBoundsPlus = p1.x + angle.x * c >= 0 &&
-                //                    p1.y + angle.y * c >= 0 &&
-                //                    p1.x + angle.x * c < limits.x &&
-                //                    p1.y + angle.y * c < limits.y;
-                withinBoundsMinus = p1.x + angle.x * -c >= 0 &&
-                                    p1.y + angle.y * -c >= 0 &&
-                                    p1.x + angle.x * -c < limits.x &&
-                                    p1.y + angle.y * -c < limits.y;
-                //if (withinBoundsPlus)
-                //{
-                //    losPoints.Add((p1.x + angle.x * c, p1.y + angle.y * c));
-                //}
-                if (withinBoundsMinus)
-                {
-                    losPoints.Add((p1.x + angle.x * -c, p1.y + angle.y * -c));
-                }
-                c++;
-            }
-            return losPoints;
+            AsteroidField afield = new AsteroidField(input);
+            List<AsteroidVisionSet> avsets = afield.GetAsteroidVisionSets();
+            return avsets.Max(x => x.VisibleAsteroidsAmount());
         }
 
         public static int SolvePartTwo()
         {
             string[] input = InputReader.StringsFromTxt("d10input.txt");
-            char[,] asteroids = new char[input[0].Length, input.Length];
-            (int x, int y) laser = (22,19);//22,19
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                char[] chars = input[i].ToCharArray();
-                for (int j = 0; j < chars.Length; j++)
-                {
-                    asteroids[j, i] = chars[j];
-                }
-            }
-            asteroids[laser.x, laser.y] = 'X';//Laser location
-
-            int width = asteroids.GetLength(0);
-            int height = asteroids.GetLength(1);
-
-            double[,] angleDegs = new double[width, height];
-            int[,] manhattans = new int[width, height];
-            Dictionary<double, List<(int, int, int)>> asteroidsAtAnglesManhattan = new Dictionary<double, List<(int, int,int)>>();
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (asteroids[x, y] == '#')
-                    {
-                        (int x, int y) diff = ((x - laser.x), (y - laser.y));
-                        (int x, int y) smallDiff = FindSmallestAngle((x, y), laser, diff);
-                        angleDegs[x, y] = DifferenceToAngle(smallDiff);
-                        manhattans[x, y] = Math.Abs(x - laser.x) + Math.Abs(y - laser.y);
-                        if (asteroidsAtAnglesManhattan.ContainsKey(angleDegs[x, y]))
-                        {
-                            asteroidsAtAnglesManhattan[angleDegs[x, y]].Add((x, y, manhattans[x, y]));
-                        } else
-                        {
-                            asteroidsAtAnglesManhattan.Add(angleDegs[x,y],new List<(int, int, int)> { (x, y, manhattans[x, y]) });
-                        }
-                    }
-                }
-            }
-
-            List<double> sortedAngles = asteroidsAtAnglesManhattan.Keys.OrderBy(x => x).ToList();
-            List<(int, int)> destroyed = new List<(int, int)>();
-            string[,] destroyedCount = new string[width, height];
-            for(int i = 0; i<height; i++)
-            {
-                for(int j = 0; j < width; j++)
-                {
-                    destroyedCount[j, i] = "0";
-                }
-            }
-
-            while (destroyed.Count < 200)
-            {
-                foreach (double angle in sortedAngles)
-                {
-                    if (asteroidsAtAnglesManhattan[angle].Count > 0)
-                    {
-                        (int x, int y, int dist) todestroy = asteroidsAtAnglesManhattan[angle].Aggregate((a, b) => a.Item3 > b.Item3 ? b : a);
-                        destroyed.Add((todestroy.x, todestroy.y));
-                        destroyedCount[todestroy.x, todestroy.y] = destroyed.Count.ToString();
-                        asteroidsAtAnglesManhattan[angle].Remove(todestroy);
-                    }
-                }
-            }
-
-            List<string> boblist = new List<string>();
-            for (int y = 0; y < height; y++)
-            {
-                string bob = "";
-                for (int x = 0; x < width; x++)
-                {
-                    if (x == laser.x && y == laser.y)
-                    {
-                        bob = bob + "|XXX|";
-                    }
-                    else
-                    {
-                        //bob = bob + (angleDegs[x, y] == 0 ? "|___|" : $"|{RoundToSignificantDigits(angleDegs[x, y], 3).ToString()}|");
-                        bob = bob + (destroyedCount[x, y] == "0" ? "|___|" : $"|{destroyedCount[x, y].ToString().PadLeft(3,' ')}|");
-                    }
-                }
-                boblist.Add(bob);
-            }
-            System.IO.File.WriteAllLines(@"../../../Resources/bob.txt", boblist.ToArray());
-            return destroyed[199].Item1 * 100 + destroyed[199].Item2;
-        }
-
-        public static double DifferenceToAngle((int x, int y) p)
-        {
-            //pythagoras
-            //double hypo = Math.Sqrt(Math.Pow(p.x, 2) + Math.Pow(p.y, 2));
-
-            //double resultingAngle = Math.Asin(p.x / hypo) * 180 / Math.PI;
-            double resultingAngle = Math.Atan2(p.y, p.x) * 180 / Math.PI;
-            //if (p.y > 0)
-            //{
-            //    resultingAngle = 180 - resultingAngle;
-            //}
-            if (p.y < 0 && p.x < 0)
-            {
-                resultingAngle = 360 + resultingAngle;
-            }
-            return resultingAngle +90;
-        }
-        public static double RoundToSignificantDigits(this double d, int digits)
-        {
-            if (d == 0)
-                return 0;
-
-            double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1);
-            return scale * Math.Round(d / scale, digits);
-        }
-
-        public static (int x,int y) FindSmallestAngle((int x, int y) p1, (int x, int y)p2, (int x, int y) diff)
-        {
-            //Find smallest angle
-            int divBy;
-            int smallestDiff = diff.x < diff.y ? diff.x : diff.y;
-            if (smallestDiff == 0)
-            {
-                divBy = Math.Max(diff.x, diff.y);
-            }
-            else
-            {
-                divBy = 1;
-                for (int i = 2; i <= smallestDiff; i++)
-                {
-                    if (diff.x % i == 0 && diff.y % i == 0)
-                    {
-                        divBy = i;
-                    }
-                }
-            }
-
-            return ((p1.x - p2.x) / divBy, (p1.y - p2.y) / divBy);
+            AsteroidField afield = new AsteroidField(input);
+            List<AsteroidVisionSet> avsets = afield.GetAsteroidVisionSets();
+            AsteroidVisionSet bestAVSet = avsets.Select(set => (set, set.VisibleAsteroidsAmount())).Aggregate((a, b) => a.Item2 > b.Item2 ? a : b).Item1;
+            Asteroid twohundredthAsteroid = bestAVSet.NthAsteroidHitByLaser(199);
+            return twohundredthAsteroid.X * 100 + twohundredthAsteroid.InvertedY;
         }
     }
 
-
-
-    public class Asteroid
+    class AsteroidField
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        List<Asteroid> asteroids;
 
-        public Asteroid(int x, int y)
+        public AsteroidField(IEnumerable<string> input)
+        {
+            List<string> asteroidRows = input.Reverse().ToList(); //Y axis starts at bottom of input
+
+            asteroids = new List<Asteroid>();
+            for (int y = 0; y < asteroidRows.Count; y++)
+            {
+                for (int x = 0; x < asteroidRows[0].Length; x++)
+                {
+                    if (asteroidRows[y][x] == '#') asteroids.Add(new Asteroid(x, y, asteroidRows.Count));
+                }
+            }
+        }
+
+        public List<AsteroidVisionSet> GetAsteroidVisionSets()
+        {
+            List<AsteroidVisionSet> avsets = new List<AsteroidVisionSet>();
+            for (int i = 0; i < asteroids.Count; i++)
+            {
+                AsteroidVisionSet avset = new AsteroidVisionSet(asteroids[i], asteroids.Take(i).Concat(asteroids.Skip(i + 1)));
+                avsets.Add(avset);
+            }
+            return avsets;
+        }
+    }
+
+    class AsteroidVisionSet
+    {
+        public Asteroid Origin;
+        List<Asteroid> otherRoids;
+        List<(int x, int y)> distFromOrigin;
+        List<(int x, int y)> smallestSteps;
+        List<double> angles;
+        List<List<int>> sortedIds;
+        List<int> bucketSizes;
+
+        public AsteroidVisionSet(Asteroid origin, IEnumerable<Asteroid> others)
+        {
+            this.Origin = origin;
+            otherRoids = new List<Asteroid>(others);
+
+            smallestSteps = otherRoids.Select(a => SmallestStepBetweenAsteroids(origin, a)).ToList();
+            angles = smallestSteps.Select(x => StepTo360Angle(x)).ToList();
+            Dictionary<double, List<int>> buckets = new Dictionary<double, List<int>>();
+            for (int i = 0; i < otherRoids.Count; i++)
+            {
+                if (!buckets.ContainsKey(angles[i]))
+                {
+                    buckets.Add(angles[i], new List<int>());
+                }
+                buckets[angles[i]].Add(i);
+            }
+
+            //Sort buckets by distance from origin
+            distFromOrigin = otherRoids.Select(a => (origin.X - a.X, origin.Y - a.Y)).ToList();
+            foreach (List<int> bucket in buckets.Values)
+            {
+                bucket.Sort((a, b) => Math.Abs(distFromOrigin[a].x).CompareTo(Math.Abs(distFromOrigin[b].x)));
+            }
+
+            sortedIds = buckets.OrderBy(kvp => kvp.Key).Select(kvp => kvp.Value).ToList();
+            bucketSizes = sortedIds.Select(x => x.Count).ToList();
+        }
+
+        public int VisibleAsteroidsAmount()
+        {
+            return sortedIds.Count;
+        }
+
+        public Asteroid NthAsteroidHitByLaser(int n)
+        {
+            int bucketsRemaining = bucketSizes.Count;
+            HashSet<int> breakPoints = bucketSizes.Distinct().ToHashSet();
+
+            int i = 0;
+            while (n > bucketsRemaining)
+            {
+                n = n - bucketsRemaining;
+                i++;
+                if (breakPoints.Contains(i))
+                {
+                    bucketsRemaining = breakPoints.Count(b => b > i);
+                }
+            }
+
+            for (int j = 0; j < sortedIds.Count; j++)
+            {
+                if (n == 0) return otherRoids[sortedIds[j][i]];
+                if (sortedIds[j].Count >= i)
+                {
+                    n--;
+                }
+            }
+
+            throw new Exception("Bad Implementation, shouldn't be possible to reach this point");
+        }
+
+        public static (int x, int y) SmallestStepBetweenAsteroids(Asteroid a1, Asteroid a2)
+        {
+            int diffX = a2.X - a1.X;
+            int diffY = a2.Y - a1.Y;
+
+            int divideBy = (int)MathHelper.GCD(diffX, diffY);
+
+            return (diffX / divideBy, diffY / divideBy);
+        }
+
+        public static double StepTo360Angle((int x, int y) step)
+        {
+            //y-axis important!! up or down increase value????
+            double resultingAngle = Math.Atan2(step.y, step.x) * 180 / Math.PI;
+            resultingAngle = resultingAngle * -1;
+            resultingAngle = (resultingAngle + 450) % 360;
+            return resultingAngle;
+        }
+    }
+
+    public struct Asteroid
+    {
+        public Asteroid(int x, int y, int yLim)
         {
             X = x;
             Y = y;
+            InvertedY = yLim - 1 - y;
         }
+
+        public int InvertedY { get; }
+        public int X { get; }
+        public int Y { get; }
+
+
+        public override string ToString() => $"({X}, {Y})";
     }
 }
 
--- AoC/Tests/Day10Tests.cs
+++ AoC/Tests/Day10Tests.cs
@@ -15,41 +15,16 @@ namespace AoC.Tests
             Console.WriteLine("Day10Tests Completed!");
         }
 
-        //public static void TestSolvePartOne()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day10.SolvePartOne();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(282);
-        //}
-
-        //public static void TestSolvePartTwo()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day10.SolvePartTwo();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(1008);
-        //}
-
         public static void TestAngleDegs()
         {
-            void Test((int x, int y) p, double expectedOutput)
-            {
-                double output = Math.Floor(Day10.DifferenceToAngle(p));
-                if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-            }
-            Test((2, -6), 18);
-            Test((0, -3), 0); //-90
-            Test((-3, 0), 270); //180
-            Test((0, 3), 180); //90
-            Test((3, 0), 90);
-            Test((2, 6), 161);
-            Test((-2, 6), 198);
-            Test((-2, -6), 341);
+            TestSuite.Test(180, (0, -1), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(270, (-1, 0), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(0, (0, 1), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(90, (1, 0), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(45, (1, 1), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(225, (-1, -1), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(315, (-1, 1), AsteroidVisionSet.StepTo360Angle);
+            TestSuite.Test(135, (1, -1), AsteroidVisionSet.StepTo360Angle);
         }
 
         static void TestSolvePartOne()
