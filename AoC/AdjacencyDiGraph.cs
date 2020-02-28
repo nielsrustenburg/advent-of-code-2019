4a92c52cd950febba1d202f98ba1d24e03f717e5
nielsrustenburg <niels.rustenburg@gmail.com>
2020-02-28 16:00:48 +0100

    Add basic directed graph implementation
    Easy to extend for challenge-specific graph objects

---
+++ AoC/AdjacencyDiGraph.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC
+{
+    abstract class AdjacencyDiGraph<N> : IInspectableDiGraph where N : IDiGraphNode
+    {
+        protected Dictionary<string, N> nodes;
+
+        protected AdjacencyDiGraph()
+        {
+            nodes = new Dictionary<string, N>();
+        }
+
+        public bool ContainsNode(string name)
+        {
+            return nodes.ContainsKey(name);
+        }
+
+        public bool ContainsEdge(string from, string to)
+        {
+            return nodes[from].HasOutNeighbour(to);
+        }
+
+        public IEnumerable<string> Nodes()
+        {
+            return nodes.Keys;
+        }
+
+        public IEnumerable<(string from, string to, int weight)> Edges()
+        {
+            foreach(var kvp in nodes)
+            {
+                foreach(var (nb, weight) in kvp.Value.OutNeighbours())
+                {
+                    yield return (kvp.Key, nb, weight);
+                }
+            }
+        }
+
+        public IEnumerable<(string nb, int weight)> InNeighbours(string name)
+        {
+            return nodes[name].InNeighbours();
+        }
+
+        public IEnumerable<(string nb, int weight)> OutNeighbours(string name)
+        {
+            return nodes[name].OutNeighbours();
+        }
+    }
+
+    abstract class AdjacencyDiGraphNode : IDiGraphNode
+    {
+        public string Name { get; }
+        Dictionary<string, int> inNeighbours;
+        Dictionary<string, int> outNeighbours;
+
+        protected AdjacencyDiGraphNode(string name)
+        {
+            Name = name;
+            inNeighbours = new Dictionary<string, int>();
+            outNeighbours = new Dictionary<string, int>();
+        }
+
+        public void AddInNeighbour(string name, int weight)
+        {
+            inNeighbours.Add(name, weight);
+        }
+
+        public void AddOutNeighbour(string name, int weight)
+        {
+            outNeighbours.Add(name, weight);
+        }
+
+        public bool HasInNeighbour(string from)
+        {
+            return inNeighbours.ContainsKey(from);
+        }
+
+        public bool HasOutNeighbour(string to)
+        {
+            return inNeighbours.ContainsKey(to);
+        }
+
+        public IEnumerable<(string nb, int weight)> InNeighbours()
+        {
+            return inNeighbours.Select(kvp => (kvp.Key, kvp.Value));
+        }
+
+        public IEnumerable<(string nb, int weight)> OutNeighbours()
+        {
+            return outNeighbours.Select(kvp => (kvp.Key, kvp.Value));
+        }
+    }
+}
