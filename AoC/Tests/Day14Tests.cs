3aa639c89f6d3e7822882a85d2baabb1886e8e63
nielsrustenburg <niels.rustenburg@gmail.com>
2020-02-28 15:59:25 +0100

    delete old commented out code

--- AoC/Tests/Day14Tests.cs
+++ AoC/Tests/Day14Tests.cs
@@ -14,26 +14,6 @@ namespace AoC.Tests
             Console.WriteLine("Day14Tests Completed!");
         }
 
-        //public static void TestSolvePartOne()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day14.SolvePartOne();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(483766);
-        //}
-
-        //public static void TestSolvePartTwo()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day14.SolvePartTwo();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(3061522);
-        //}
-
         static void TestSolvePartOne()
         {
             TestSuite.Test(483766, Day14.SolvePartOne);
