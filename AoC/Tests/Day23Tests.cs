7600a0c1a059bfc0ae30ddba1d71e50b2fd6b94b
nielsrustenburg <niels.rustenburg@gmail.com>
2020-03-02 13:02:14 +0100

    remove old commented out tests

--- AoC/Tests/Day23Tests.cs
+++ AoC/Tests/Day23Tests.cs
@@ -15,28 +15,6 @@ namespace AoC.Tests
             Console.WriteLine("Day23Tests Completed!");
         }
 
-        //public static void TestSolvePartOne()
-        //{
-        //    void Test(string expected)
-        //    {
-        //        BigInteger expectedOutput = BigInteger.Parse(expected);
-        //        BigInteger output = Day23.SolvePartOne();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test("24555");
-        //}
-
-        //public static void TestSolvePartTwo()
-        //{
-        //    void Test(string expected)
-        //    {
-        //        BigInteger expectedOutput = BigInteger.Parse(expected);
-        //        BigInteger output = Day23.SolvePartTwo();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test("19463");
-        //}
-
         static void TestSolvePartOne()
         {
             TestSuite.Test("24555", Day23.SolvePartOne);
