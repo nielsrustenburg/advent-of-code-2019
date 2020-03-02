5e3c33866251515a5e386cb9ff39adcb2a788c54
nielsrustenburg <niels.rustenburg@gmail.com>
2020-03-02 12:47:20 +0100

    remove old commented out tests

--- AoC/Tests/Day19Tests.cs
+++ AoC/Tests/Day19Tests.cs
@@ -14,26 +14,6 @@ namespace AoC.Tests
             Console.WriteLine("Day19Tests Completed!");
         }
 
-        //public static void TestSolvePartOne()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day19.SolvePartOne();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(201);
-        //}
-
-        //public static void TestSolvePartTwo()
-        //{
-        //    void Test(int expectedOutput)
-        //    {
-        //        int output = Day19.SolvePartTwo();
-        //        if (output != expectedOutput) throw new Exception($"{TestSuite.GetCurrentMethod()}(): {output}, expected {expectedOutput}");
-        //    }
-        //    Test(6610984);
-        //}
-
         static void TestSolvePartOne()
         {
             TestSuite.Test(201, Day19.SolvePartOne);
