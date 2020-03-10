0eb691f76f72948170f9f85c034927ff549340f8
nielsrustenburg <niels.rustenburg@gmail.com>
2020-03-10 12:15:44 +0100

    change outdated & unfinished code to comments
    I'm not removing this for now because I might want to continue with the same idea, even if I won't reuse any of this code

--- AoC/Days/Day25.cs
+++ AoC/Days/Day25.cs
@@ -69,161 +69,161 @@ namespace AoC
         }
     }
 
-    //Had the idea to do an automated solution for this exercise but didn't finish it
-    //Leaving this just in case I feel like continuing
+    ////Had the idea to do an automated solution for this exercise but didn't finish it
+    ////Leaving this just in case I feel like continuing
 
-    class DroidSquad
-    {
-        Dictionary<string, string> itemsToRooms;
-        Dictionary<string, Node> roomNameToNode;
-    }
+    //class DroidSquad
+    //{
+    //    Dictionary<string, string> itemsToRooms;
+    //    Dictionary<string, Node> roomNameToNode;
+    //}
 
-    class ScoutDroid : ASCIIComputer
-    {
-        Room currentRoom;
-        FloorPlan knownRooms;
+    //class ScoutDroid : ASCIIComputer
+    //{
+    //    Room currentRoom;
+    //    FloorPlan knownRooms;
 
-        public ScoutDroid(IEnumerable<BigInteger> programming) : base(programming)
-        {
-            knownRooms = new FloorPlan();
-            string output = RunString();
-            knownRooms.Update(AnalyzeSurroundings(output));
-        }
+    //    public ScoutDroid(IEnumerable<BigInteger> programming) : base(programming)
+    //    {
+    //        knownRooms = new FloorPlan();
+    //        string output = RunString();
+    //        knownRooms.Update(AnalyzeSurroundings(output));
+    //    }
 
-        public void Explore()
-        {
-            while (knownRooms.HasUnexploredDoors.Any())
-            {
-                FloorPlan.GetPathTo(currentRoom.Name, knownRooms.HasUnexploredDoors.Last());
-            }
-        }
+    //    public void Explore()
+    //    {
+    //        while (knownRooms.HasUnexploredDoors.Any())
+    //        {
+    //            FloorPlan.GetPathTo(currentRoom.Name, knownRooms.HasUnexploredDoors.Last());
+    //        }
+    //    }
 
-        public List<(string content, string tag)> AnalyzeSurroundings(string surroundings)
-        {
-            string[] lines = surroundings.Split("\n");
-            List<(string content, string tag)> analysis = new List<(string content, string tag)>();
-            int i = 0;
-            while (i < 3)
-            {
-                if (lines[i] != "") throw new Exception("Unexpected line");
-                i++;
-            }
-            if (lines[i].First() == '=')
-            {
-                analysis.Add((lines[i], "Name"));
-                analysis.Add((lines[i + 1], "Flavor"));
-                i += 4;
-            }
-            else
-            {
-                throw new Exception("Not a room");
-            }
-            if (lines[i - 1] != "Doors here lead:") throw new Exception("Unexpected line");
-            while (lines[i] != "")
-            {
-                string door = lines[i].Substring(2);
-                analysis.Add((door, "Door"));
-                i++;
-            }
+    //    public List<(string content, string tag)> AnalyzeSurroundings(string surroundings)
+    //    {
+    //        string[] lines = surroundings.Split("\n");
+    //        List<(string content, string tag)> analysis = new List<(string content, string tag)>();
+    //        int i = 0;
+    //        while (i < 3)
+    //        {
+    //            if (lines[i] != "") throw new Exception("Unexpected line");
+    //            i++;
+    //        }
+    //        if (lines[i].First() == '=')
+    //        {
+    //            analysis.Add((lines[i], "Name"));
+    //            analysis.Add((lines[i + 1], "Flavor"));
+    //            i += 4;
+    //        }
+    //        else
+    //        {
+    //            throw new Exception("Not a room");
+    //        }
+    //        if (lines[i - 1] != "Doors here lead:") throw new Exception("Unexpected line");
+    //        while (lines[i] != "")
+    //        {
+    //            string door = lines[i].Substring(2);
+    //            analysis.Add((door, "Door"));
+    //            i++;
+    //        }
 
-            if (lines[i + 1] == "Items here:")
-            {
-                i += 2;
-                while (lines[i] != "")
-                {
-                    string item = lines[i].Substring(2);
-                    analysis.Add((item, "Item"));
-                    i++;
-                }
-            }
+    //        if (lines[i + 1] == "Items here:")
+    //        {
+    //            i += 2;
+    //            while (lines[i] != "")
+    //            {
+    //                string item = lines[i].Substring(2);
+    //                analysis.Add((item, "Item"));
+    //                i++;
+    //            }
+    //        }
 
-            if (lines[i + 1] == "Command?") return analysis;
-            throw new Exception("Unexpected surroundings");
-        }
-    }
+    //        if (lines[i + 1] == "Command?") return analysis;
+    //        throw new Exception("Unexpected surroundings");
+    //    }
+    //}
 
-    class FloorPlan
-    {
-        Dictionary<string, Room> rooms;
-        public List<string> HasUnexploredDoors { get; private set; }
+    //class FloorPlan
+    //{
+    //    Dictionary<string, Room> rooms;
+    //    public List<string> HasUnexploredDoors { get; private set; }
 
-        public FloorPlan()
-        {
-            rooms = new Dictionary<string, Room>();
-            HasUnexploredDoors = new List<string>();
-        }
+    //    public FloorPlan()
+    //    {
+    //        rooms = new Dictionary<string, Room>();
+    //        HasUnexploredDoors = new List<string>();
+    //    }
 
-        internal Room Update(List<(string content, string tag)> roomInfo)
-        {
-            string name = roomInfo.Find(x => x.tag == "Name").content;
-            if (rooms.ContainsKey(name))
-            {
-                //WE ALREADY KNOW THIS ROOM??
-                return rooms[name];
-            }
-            else
-            {
-                Room newRoom = new Room(roomInfo);
-                rooms.Add(name, newRoom);
-                HasUnexploredDoors.Add(name);
-                return newRoom;
-            }
-        }
+    //    internal Room Update(List<(string content, string tag)> roomInfo)
+    //    {
+    //        string name = roomInfo.Find(x => x.tag == "Name").content;
+    //        if (rooms.ContainsKey(name))
+    //        {
+    //            //WE ALREADY KNOW THIS ROOM??
+    //            return rooms[name];
+    //        }
+    //        else
+    //        {
+    //            Room newRoom = new Room(roomInfo);
+    //            rooms.Add(name, newRoom);
+    //            HasUnexploredDoors.Add(name);
+    //            return newRoom;
+    //        }
+    //    }
 
-        internal void Update(List<(string content, string tag)> roomInfo, string previousRoom, string moveOrder)
-        {
-            Room updateRoom = Update(roomInfo);
-            //Update previous rooms knowledge
-            bool prevRoomExploredCompletely = rooms[previousRoom].UpdateDoorKnowledge(moveOrder, updateRoom.Name);
-            if (prevRoomExploredCompletely) HasUnexploredDoors.Remove(previousRoom);
+    //    internal void Update(List<(string content, string tag)> roomInfo, string previousRoom, string moveOrder)
+    //    {
+    //        Room updateRoom = Update(roomInfo);
+    //        //Update previous rooms knowledge
+    //        bool prevRoomExploredCompletely = rooms[previousRoom].UpdateDoorKnowledge(moveOrder, updateRoom.Name);
+    //        if (prevRoomExploredCompletely) HasUnexploredDoors.Remove(previousRoom);
 
-            //Update new rooms knowledge
-            bool exploredFully = updateRoom.UpdateDoorKnowledge(OppositeDirection(moveOrder), previousRoom);
-            if (exploredFully) HasUnexploredDoors.Remove(updateRoom.Name);
-        }
+    //        //Update new rooms knowledge
+    //        bool exploredFully = updateRoom.UpdateDoorKnowledge(OppositeDirection(moveOrder), previousRoom);
+    //        if (exploredFully) HasUnexploredDoors.Remove(updateRoom.Name);
+    //    }
 
-        public string OppositeDirection(string direction)
-        {
-            if (direction == "east") return "west";
-            if (direction == "west") return "east";
-            if (direction == "north") return "south";
-            if (direction == "south") return "north";
-            throw new Exception("not a valid direction");
-        }
+    //    public string OppositeDirection(string direction)
+    //    {
+    //        if (direction == "east") return "west";
+    //        if (direction == "west") return "east";
+    //        if (direction == "north") return "south";
+    //        if (direction == "south") return "north";
+    //        throw new Exception("not a valid direction");
+    //    }
 
-        internal static void GetPathTo(string from, string to)
-        {
-            //BFS 
-            throw new NotImplementedException();
-        }
-    }
+    //    internal static void GetPathTo(string from, string to)
+    //    {
+    //        //BFS 
+    //        throw new NotImplementedException();
+    //    }
+    //}
 
-    class Room
-    {
-        public string Name { get; private set; }
-        string flavorText;
-        List<string> items;
-        Dictionary<string, string> doors;
+    //class Room
+    //{
+    //    public string Name { get; private set; }
+    //    string flavorText;
+    //    List<string> items;
+    //    Dictionary<string, string> doors;
 
-        public Room(List<(string content, string tag)> roomInfo)
-        {
-            items = new List<string>();
-            doors = new Dictionary<string, string>();
-            foreach (var info in roomInfo)
-            {
-                if (info.tag == "Name") Name = info.content;
-                if (info.tag == "Flavor") flavorText = info.content;
-                if (info.tag == "Item") items.Add(info.content);
-                if (info.tag == "Door") doors.Add(info.content, "unexplored");
-            }
-        }
+    //    public Room(List<(string content, string tag)> roomInfo)
+    //    {
+    //        items = new List<string>();
+    //        doors = new Dictionary<string, string>();
+    //        foreach (var info in roomInfo)
+    //        {
+    //            if (info.tag == "Name") Name = info.content;
+    //            if (info.tag == "Flavor") flavorText = info.content;
+    //            if (info.tag == "Item") items.Add(info.content);
+    //            if (info.tag == "Door") doors.Add(info.content, "unexplored");
+    //        }
+    //    }
 
-        public bool UpdateDoorKnowledge(string direction, string roomName)
-        {
-            if (doors[direction] != "unexplored") throw new Exception("Already knew this one");
-            doors[direction] = roomName;
-            return !doors.ContainsValue("unexplored");
-        }
-    }
+    //    public bool UpdateDoorKnowledge(string direction, string roomName)
+    //    {
+    //        if (doors[direction] != "unexplored") throw new Exception("Already knew this one");
+    //        doors[direction] = roomName;
+    //        return !doors.ContainsValue("unexplored");
+    //    }
+    //}
 }
 
