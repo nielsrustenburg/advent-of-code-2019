58d642f3c09336c0b7ee0056512c4aadb29a910d
nielsrustenburg <niels.rustenburg@gmail.com>
2020-03-16 12:44:36 +0100

    add InputParser for parsing strings from inputfiles

---
+++ AoC/Utils/InputParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace AoC.Utils
+{
+    static class InputParser
+    {
+        public static IEnumerable<int> ParseInts(IEnumerable<string> lines)
+        {
+            return InputParser<int>.ParseLines(lines, int.Parse);
+        }
+
+        public static IEnumerable<BigInteger> ParseBigIntegers(IEnumerable<string> lines)
+        {
+            return InputParser<BigInteger>.ParseLines(lines, BigInteger.Parse);
+        }
+
+        public static IEnumerable<IEnumerable<string>> ParseCSVStrings(IEnumerable<string> lines)
+        {
+            return InputParser<IEnumerable<string>>.ParseLines(lines, ParseCSVLine);
+        }
+
+        public static IEnumerable<string> ParseCSVLine(string line)
+        {
+            return line.Split(',');
+        }
+    }
+
+    static class InputParser<T>
+    {
+        public static IEnumerable<T> ParseLines(IEnumerable<string> lines, Func<string, T> parse)
+        {
+            return lines.Select(l => parse(l));
+        }
+    }
+}
